b8611b7ff8029936e327a04c6aada09e5a661730
AbdooMEid <63204388+AbdooMEid@users.noreply.github.com>
2022-04-08 20:22:45 +0200

    Chcek Time
    Chcek Time

--- Apis/edit.routes.js
+++ Apis/edit.routes.js
@@ -9,8 +9,14 @@ const auth = require('../auth/auth')
 
 app.put('/updateNote'  , async (req,res)=>{
     try {
-        await noteModel.findByIdAndUpdate({_id : req.body.edit} , {title : req.body.title , desc : req.body.desc , Time : req.body.Time})
+const {title , desc , Time} = req.body
+        const time = await noteModel.findOne({Time});
+        if(time === null){
+         await noteModel.findByIdAndUpdate({_id : req.body.edit} , {title , desc , Time})
         res.status(200).json({message : 'Updated'})
+        }else{
+            res.status(201).json({'message' : 'time is repeted'})
+        }
     } catch (error) {
         res.status(201).json({error})
     }
